fa43e4dfe009ca8f07dfc0191fb6af18e14ef013
Vinci Rufus <vinci.rufus@gmail.com>
2016-12-03 01:07:18 +0530

    adds material ui components

--- js/app.js
+++ js/app.js
@@ -5,12 +5,18 @@ import AppHomeRoute from './routes/AppHomeRoute';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
+          <MuiThemeProvider>
+
   <Relay.Renderer
     environment={Relay.Store}
     Container={App}
     queryConfig={new AppHomeRoute()}
-  />,
+  />
+          </MuiThemeProvider>
+
+  ,
   document.getElementById('root')
 );
--- js/components/App.js
+++ js/components/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import Relay from 'react-relay';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import './app.css';
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Twitterati</h1>
-          <ul>
-            {this.props.tweet.tweet.map(tweet =>
-              <li key={tweet.id}><small>id: {tweet.id}</small> <br></br> {tweet.text}  <br></br>-- @<i>{tweet.user.screen_name}</i></li>
+        <h1>Twitteratis</h1>
+            {this.props.tweet.tweet.map(tweet =>  
+            //  <span key={tweet.id}><small>id: {tweet.id}</small> <br></br> {tweet.text}  <br></br>-- @<i>{tweet.user.screen_name}</i></span>
+           
+           <Card
+           style={{
+             margin:'5px 3px'
+           }}
+           >
+           
+    <CardHeader
+      title={tweet.user.screen_name}
+avatar={tweet.user.profile_image_url}
+subtitle={tweet.created_at}    
+    />
+    <CardText>
+     {tweet.text}+ {tweet.user.profile_image_url}
+    </CardText>
+  </Card>
+
             )}
-          </ul>
+
       </div>
     );
   }
@@ -24,8 +42,10 @@ export default Relay.createContainer(App, {
         tweet{
           id,
           text,
+          created_at
           user{
-            screen_name
+            screen_name,
+            profile_image_url
           }
         }
       }
