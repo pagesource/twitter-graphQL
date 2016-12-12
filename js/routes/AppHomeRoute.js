a7a967463f205cf16b25742cdac3776eb3753e0f
shafeeqonline <shafeeq.rahman01@gmail.com>
2016-12-12 22:36:52 +0530

    Removing unwanted styles

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -4,7 +4,7 @@ export default class extends Relay.Route {
   static queries = {
     tweet: () => Relay.QL`
       query TwitterAPI{
-        search(q: "reactjs")
+        search(q: "donaldtrump")
       }
     `,
   };
