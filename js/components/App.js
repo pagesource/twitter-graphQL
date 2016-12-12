0239f986d186cafdb99d2693ad059d58e00a37ea
shafeeqonline <shafeeq.rahman01@gmail.com>
2016-12-12 22:22:11 +0530

    Removing react errors for card components to become efficient

--- js/components/App.js
+++ js/components/App.js
@@ -11,18 +11,18 @@ class App extends React.Component {
       <div>
 
         <h1>Twitteratis</h1>
-            {this.props.tweet.tweet.map(tweet =>  
+            {this.props.tweet.tweet.map(tweet =>
             //  <span key={tweet.id}><small>id: {tweet.id}</small> <br></br> {tweet.text}  <br></br>-- @<i>{tweet.user.screen_name}</i></span>
-           
-           <Card
+
+           <Card key={tweet.id}
            style={{
              margin:'15px 3px'
            }}
            >
-           
+
 
     <CardText
-    styles={{
+    style={{
       fontSize:'22px'
     }}>
      {tweet.text}+ {tweet.user.profile_image_url}
